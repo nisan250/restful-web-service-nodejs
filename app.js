d351c96568aecb1e26af4e67f8b017e558b43ae4
nisan250 <nisan250@gmail.com>
2019-03-16 17:51:27 +0000

    http get -getting all books, all books by genre and a single book

--- app.js
+++ app.js
@@ -7,18 +7,33 @@ const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel');
 
-bookRouter.route('/books').get((req, res) => {
-  // const response = { hello: 'This is my API' };
-
-  // res.json(response);
-
-  Book.find((err, books) => {
-    if (err) {
-      return res.send(err);
+// ----- get all books or all books with a specific genre ----- //
+bookRouter.route('/books')
+  .get((req, res) => {
+    // const response = { hello: 'This is my API' };
+    // res.json(response);
+    const query = {};
+    if (req.query.genre) {
+      query.genre = req.query.genre;
     }
-    return res.json(books);
+    Book.find(query, (err, books) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(books);
+    });
   });
-});
+
+// ----- get single book ----- //
+bookRouter.route('/books/:bookId')
+  .get((req, res) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(book);
+    });
+  });
 
 app.use('/api', bookRouter);
 

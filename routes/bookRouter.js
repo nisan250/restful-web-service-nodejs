f2b495b0b9f3c74465a9d6ad0864a4df4ce1ee59
nisan250 <nisan250@gmail.com>
2019-03-17 09:16:51 +0000

    adding middleware

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -22,24 +22,35 @@ function routes(Book) {
         return res.json(books);
       });
     });
+
+  bookRouter.use('/books/:bookId', (req, res, next) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err) {
+        return res.send(err);
+      }
+      if (book) {
+        req.book = book;
+        return next();
+      }
+
+      return res.sendStatus(404);
+    });
+  });
   bookRouter.route('/books/:bookId')
-    .get((req, res) => {
-      Book.findById(req.params.bookId, (err, book) => {
+    .get((req, res) => res.json(req.book))
+    .put((req, res) => {
+      const { book } = req;
+      book.title = req.body.title;
+      book.author = req.body.author;
+      book.genre = req.body.genre;
+      book.read = req.body.read;
+      // book.save();
+      // return res.json(book);
+
+      req.book.save((err) => {
         if (err) {
           return res.send(err);
         }
-        return res.json(book);
-      });
-    }).put((req, res) => {
-      Book.findById(req.params.bookId, (err, book) => {
-        if (err) {
-          return res.send(err);
-        }
-        book.title = req.body.title;
-        book.author = req.body.author;
-        book.genre = req.body.genre;
-        book.read = req.body.read;
-        book.save();
         return res.json(book);
       });
     });

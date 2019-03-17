e2f840fc669e773659d09a51c046167079de1255
nisan250 <nisan250@gmail.com>
2019-03-17 07:52:16 +0000

    Posting Data: saving book in books

--- app.js
+++ app.js
@@ -1,44 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI');
-const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(Book);
 
-// ----- get all books or all books with a specific genre ----- //
-bookRouter.route('/books')
-  .get((req, res) => {
-    // const response = { hello: 'This is my API' };
-    // res.json(response);
-    const query = {};
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    }
-    Book.find(query, (err, books) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(books);
-    });
-  });
-
-// ----- get single book ----- //
-bookRouter.route('/books/:bookId')
-  .get((req, res) => {
-    Book.findById(req.params.bookId, (err, book) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(book);
-    });
-  });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
-  res.send('Welcome to my API!');
+  res.send('Welcome to my Nodemon API!');
 });
 
 app.listen(port, () => {

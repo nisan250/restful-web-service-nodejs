9df60a3889860cc71db97956115d3e9f559ccab6
nisan250 <nisan250@gmail.com>
2019-03-17 15:26:13 +0000

    implementing HATEOS (HyperMedia)

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -18,7 +18,13 @@ function booksController(Book) {
       if (err) {
         return res.send(err);
       }
-      return res.json(books);
+      const returnBooks = books.map((book) => {
+        const newBook = book.toJSON();
+        newBook.links = {};
+        newBook.links.self = `http://${req.headers.host}/api/books/${book._id}`;
+        return newBook;
+      });
+      return res.json(returnBooks);
     });
   }
 
